9282d4fe69d82478eebf41b2d74420e36f8cd10d
James Langdon <corporatelangdon@gmail.com>
2026-01-05 21:20:21 -0500

    add MultiUserOAuthTokenStorageManager

--- Sources/VLOAuthFlowCoordinator/Public/OAuthTokenStorageManager.swift
+++ Sources/VLOAuthFlowCoordinator/Public/OAuthTokenStorageManager.swift
@@ -5,50 +5,43 @@
 //  Created by James Langdon on 11/23/25.
 //
 
-public class OAuthTokenStorageManager {
-    private enum TokenType: String {
+class OAuthTokenStorageManager {
+    enum TokenType: String {
         case accessToken = "oauth_access_token"
         case accessTokenSecret = "oauth_access_token_secret"
         case requestTokenSecret = "oauth_request_token_secret"
     }
     
-    public init() {}
+    let keychainManager: KeychainManager
+    
+    init(keychainManager: KeychainManager) {
+        self.keychainManager = keychainManager
+    }
     
     // MARK: - Save Tokens
-    func saveAccessToken(_ token: String) -> Bool {
-        return KeychainHelper.save(key: TokenType.accessToken.rawValue, string: token)
+    func save(_ token: String, for tokenType: TokenType) -> Bool {
+        save(token, for: tokenType.rawValue)
     }
     
-    func saveAccessTokenSecret(_ token: String) -> Bool {
-        return KeychainHelper.save(key: TokenType.accessTokenSecret.rawValue, string: token)
-    }
-    
-    func saveRequestTokenSecret(_ secret: String) -> Bool {
-        return KeychainHelper.save(key: TokenType.requestTokenSecret.rawValue, string: secret)
+    func save(_ token: String, for key: String) -> Bool {
+        keychainManager.save(key: key, string: token)
     }
     
     // MARK: - Get Tokens
-    func getAccessToken() -> String? {
-        return KeychainHelper.loadString(key: TokenType.accessToken.rawValue)
+    func get(tokenType: TokenType) -> String? {
+        getToken(for: tokenType.rawValue)
     }
     
-    func getAccessTokenSecret() -> String? {
-        return KeychainHelper.loadString(key: TokenType.accessTokenSecret.rawValue)
+    func getToken(for key: String) -> String? {
+        keychainManager.loadString(key: key)
     }
     
-    func getRequestTokenSecret() -> String? {
-        return KeychainHelper.loadString(key: TokenType.requestTokenSecret.rawValue)
+    // MARK: - Delete Tokens
+    func delete(tokenType: TokenType) -> Bool {
+        deleteToken(for: tokenType.rawValue)
     }
     
-    // MARK: - Clear Tokens
-    func clearAllTokens() {
-        KeychainHelper.delete(key: TokenType.accessToken.rawValue)
-        KeychainHelper.delete(key: TokenType.accessTokenSecret.rawValue)
-        KeychainHelper.delete(key: TokenType.requestTokenSecret.rawValue)
-    }
-    
-    // MARK: - Token Validation
-    func hasValidTokens() -> Bool {
-        return getAccessToken() != nil && getRequestTokenSecret() != nil
+    func deleteToken(for key: String) -> Bool {
+        keychainManager.delete(key: key)
     }
 }
